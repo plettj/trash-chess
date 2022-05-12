4392009b48f39fae69ad227d3b87477dc06ac3b3
Josiah Plett <65507438+plettj@users.noreply.github.com>
2022-05-11 21:50:09 -0700

    First Personal React App!

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Chessboard from "chessboardjsx";
+import { ChessInstance, ShortMove } from "chess.js";
+import * as ChessJS from "chess.js";
 
-function App() {
+const Chess = typeof ChessJS === "function" ? ChessJS : ChessJS.Chess;
+
+const TrashChess = (): JSX.Element => {
+  const [board] = useState<ChessInstance>(
+    new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+  );
+  const [fen, setFen] = useState(board.fen());
+
+  const handleMove = (move: ShortMove) => {
+    if (board.move(move)) {
+      setTimeout(() => {
+        const moves = board.moves();
+
+        if (moves.length > 0) {
+          const computerMove = moves[Math.floor(Math.random() * moves.length)];
+          board.move(computerMove);
+          setFen(board.fen());
+        }
+      }, 200);
+
+      setFen(board.fen());
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="screen-box">
+      <h1>Trash Chess Engine</h1>
+      <section className="middle">
+        <Chessboard
+          position={fen}
+          dropSquareStyle={{boxShadow: 'inset 0 0 1px 4px rgba(0, 0, 0, 0.2)'}}
+          onDrop={(move) =>
+            handleMove({
+              from: move.sourceSquare,
+              to: move.targetSquare,
+              promotion: "q",
+            })
+          }
+        />
+      </section>
     </div>
   );
 }
 
-export default App;
+export default TrashChess;
