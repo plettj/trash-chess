1e2bc533a100b1f059e3910ec204a18cf8d765df
Josiah Plett <65507438+plettj@users.noreply.github.com>
2022-05-14 02:04:36 -0700

    Interface for the app is starting to develop!

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,35 @@
-import React, { useState } from "react";
 import "./App.css";
-import Chessboard from "chessboardjsx";
-import { ChessInstance, ShortMove } from "chess.js";
-import * as ChessJS from "chess.js";
+import { Box } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import TrashChess from "./Root";
 
-const Chess = typeof ChessJS === "function" ? ChessJS : ChessJS.Chess;
-
-const TrashChess = (): JSX.Element => {
-  const [board] = useState<ChessInstance>(
-    new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-  );
-  const [fen, setFen] = useState(board.fen());
-
-  const handleMove = (move: ShortMove) => {
-    if (board.move(move)) {
-      setTimeout(() => {
-        const moves = board.moves();
-
-        if (moves.length > 0) {
-          const computerMove = moves[Math.floor(Math.random() * moves.length)];
-          board.move(computerMove);
-          setFen(board.fen());
-        }
-      }, 200);
-
-      setFen(board.fen());
+const mainTheme = createTheme({
+  spacing: 4,
+  palette: {
+    primary: {
+      main: '#ed1334',
+      light: '#f24962',
+      dark: '#bf0d28',
+      contrastText: '#fcfafa'
+    },
+    secondary: {
+      main: '#E33E7F'
     }
-  };
+  }
+});
 
+console.log(mainTheme);
+
+const App = (): JSX.Element => {
   return (
-    <div className="screen-box">
-      <h1>Trash Chess Engine</h1>
-      <section className="middle">
-        <Chessboard
-          position={fen}
-          dropSquareStyle={{boxShadow: 'inset 0 0 1px 4px rgba(0, 0, 0, 0.2)'}}
-          onDrop={(move) =>
-            handleMove({
-              from: move.sourceSquare,
-              to: move.targetSquare,
-              promotion: "q",
-            })
-          }
-        />
-      </section>
-    </div>
+    <ThemeProvider theme={mainTheme}>
+      <Box height="100vh" display="flex" flexDirection="column">
+        <Box flex={1} overflow="auto">
+          <TrashChess />
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
 
-export default TrashChess;
+export default App;
