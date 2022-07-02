e5d60785eae10b563dfec9cda904ed59ada967be
Josiah Plett <jlplett303@gmail.com>
2022-07-02 13:43:50 -0700

    TrashChess -> TSChessEngine

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -4,9 +4,9 @@ export default function TopBar() {
   return (
     <AppBar>
       <Toolbar>
-        <Button color="inherit" href="https://www.plett.dev/">Website</Button>
+        <Button color="inherit" href="https://www.plett.dev/">Portfolio</Button>
         <Typography variant="h4" component="div" align="center" sx={{ flexGrow: 1 }}>
-          Trash Chess
+          TS Chess Engine
         </Typography>
         <Button color="inherit" href="https://github.com/plettj">Github</Button>
       </Toolbar>
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import TrashChess from './TrashChess';
+import TSChessEngine from './TSChessEngine';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   //<React.StrictMode>
-    <TrashChess />
+    <TSChessEngine />
   //</React.StrictMode>
 );
